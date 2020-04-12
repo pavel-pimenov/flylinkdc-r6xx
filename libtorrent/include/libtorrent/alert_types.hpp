af41ed0fa15ac1925b62d70fe478a9eec4f017ec
Pave Pimenov <pavel.pimenov@gmail.com>
2020-04-12 13:55:49 +0300

    * jsoncpp * mediainfolib

--- libtorrent/include/libtorrent/alert_types.hpp
+++ libtorrent/include/libtorrent/alert_types.hpp
@@ -2777,6 +2777,7 @@ TORRENT_VERSION_NAMESPACE_2
 	{
 		// internal
 		TORRENT_UNEXPORT dht_sample_infohashes_alert(aux::stack_allocator& alloc
+			, sha1_hash const& nid
 			, udp::endpoint const& endp
 			, time_duration interval
 			, int num
@@ -2787,6 +2788,9 @@ TORRENT_VERSION_NAMESPACE_2
 		TORRENT_DEFINE_ALERT(dht_sample_infohashes_alert, 93)
 
 		std::string message() const override;
+
+		// id of the node the request was sent to (and this response was received from)
+		sha1_hash node_id;
 
 		// the node the request was sent to (and this response was received from)
 		aux::noexcept_movable<udp::endpoint> endpoint;
