66a8d27f75af0cbb53de20e1a63c3c47508084df
Pave Pimenov <pavel.pimenov@gmail.com>
2021-02-13 15:31:48 +0300

    * cleanup

--- build_all_2017.bat
+++ build_all_2017.bat
@@ -5,12 +5,6 @@ call build_flylinkdc_x64_2017.bat
 
 if not exist .\compiled\FlylinkDC*.exe goto :builderror
 
-copy FlylinkDC-*-x64-*build-*-*.7z "D:\ppa-doc\mailru\flylinkdc\r6xx\x64"
-
-move *-debug-info-*.7z "U:\webdav\src-bin-pdb\flylinkdc-r6xx-pdb"
-move *-src-*.7z "U:\webdav\src-bin-pdb\flylinkdc-r6xx\r600"
-move *.7z "U:\webdav\src-bin-pdb\flylinkdc-r6xx\r600"
-
 cd setup 
 rem call build_setup_base.bat
 cd ..
