aaee9176abdd024e65ddef6580f5c35f0efc1ecd
Pave Pimenov <pavel.pimenov@gmail.com>
2023-01-04 12:09:08 +0300

    * update lib

--- libtorrent/src/torrent.cpp
+++ libtorrent/src/torrent.cpp
@@ -5746,6 +5746,12 @@ namespace {
 		}
 	}
 
+	void torrent::post_trackers()
+	{
+		auto t = trackers();
+		m_ses.alerts().emplace_alert<tracker_list_alert>(get_handle(), std::move(t));
+	}
+
 	std::vector<lt::announce_entry> torrent::trackers() const
 	{
 		std::vector<lt::announce_entry> ret;
@@ -11583,6 +11589,14 @@ namespace {
 #endif
 
 		m_links[aux::session_interface::torrent_state_updates].insert(list, this);
+	}
+
+	void torrent::post_status(status_flags_t const flags)
+	{
+		std::vector<torrent_status> s;
+		s.resize(1);
+		status(&s.front(), flags);
+		m_ses.alerts().emplace_alert<state_update_alert>(std::move(s));
 	}
 
 	void torrent::status(torrent_status* st, status_flags_t const flags)
