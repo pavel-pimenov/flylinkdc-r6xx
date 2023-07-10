fbc999ac73d4a9add4fccac6e7c7179199cc41c9
Pave Pimenov <pavel.pimenov@gmail.com>
2023-07-10 03:53:00 +0300

    * build-23246

--- revision.h
+++ revision.h
@@ -2,7 +2,7 @@
 #define FLY_REVISION_H
  
 #define VERSION_NUM 601
-#define REVISION_NUM 23242
+#define REVISION_NUM 23246
 #define FLYLINKDC_BETA
 #define BETA_NUM   1 // Number of beta. Does not matter if the #define FLYLINKDC_BETA is disabled.
  
